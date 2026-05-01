45d9554cd21611e519d30abe5a308741491a6f0f
André Miranda <39061067+andreemiranda@users.noreply.github.com>
2026-05-01 11:36:57 -0300

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-window.onload=e=>{
-    document.querySelector("video").muted=true;
-    //document.querySelector("video").style.width=document.body.offsetHeight+"px"
-document.querySelector("video").play()
-.then(res=>{console.log(res)})
-}
